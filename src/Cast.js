d27b53e7082182394c7c42008dd751ea11847f4a
Charles Jones <c.jones.3.zero@gmail.com>
2019-07-30 18:59:19 -0600

    Adding static quotes

--- src/Cast.js
+++ src/Cast.js
@@ -6,6 +6,7 @@ export default class Cast extends React.Component {
 
     this.state = {
       res1: [],
+      quotes: [],
     }
   }
 
@@ -17,18 +18,53 @@ export default class Cast extends React.Component {
     .then(res => res.json())
     .then(res => {
       this.setState({
-        res1: res.cast
+        res1: res.cast,
+        quotesDennis: [
+          `I am a golden god!`,
+          `Without the sunglasses, Weekend at Bernie's would have been a very dark, strange tale.`,
+          `So, all for thinner limes, raise your hands. And all for thicker limes, raise your hands. Now, there you go. That's a democracy in action. That's two votes against two votes. A perfect example of when democracy has failed.`,
+          `My nose was chiseled by the god themselves, Frank. My body was scuplted to the proportions of Michaelango's "David". You, on the other hand, are a pit of despair. `,
+        ],
+        quotesCharlie: [
+          `I'll just regress, because I feel I've made myself perfectly redundant.`,
+          `Aw yeah, yeah, like in The Sixth Sense you find out that the dude in that hair piece the whole time, that's Bruce Willis the whole movie.`,
+          `Just get a job? Why don’t I strap on my job helmet, and squeeze down into a job cannon, AND FIRE OFF INTO JOBLAND, WHERE JOBS GROW ON JOBBIES?!!!`,
+          `Keep singing, bitch! You're not gonna have a face by the time I'm through with you!`,
+        ],
+        quotesDeandra: [
+          `*Ostrich Noises*`,
+          `*Ostrich Noises*`,
+          `*Ostrich Noises*`,
+          `*Ostrich Noises*`,
+        ],
+        quotesFrank: [
+          `Actually it is Mantis. Mantis Toboggan, MD.`,
+          `I still. Got the. Moooooooooves. Go for it, go for it!`,
+          `Ongo goblogian, the art collector! Charmed, I'm sure! I'm going to invite you to a show, but first, allow me to destroy your gallery.`,
+          `Well, I don't know how many years on this Earth I got left, but I'm gonna get real weird with it.`,
+        ],
+        quotesRonald: [
+          `But I'm more healthier than he is, is the point I think you're trying to make, even with the diabitis`,
+          `2 -R`,
+          `3 -R`,
+          `4 -R`,
+        ]
       })
-      console.log(this.res1);
+
+
+      // this.state.res1[0]['quotes'] = [];
+      // this.state.res1[0]['quotes'][0] = `I'm Dennis Reynolds 2`;
+      // this.state.res1[0]['quotes'][1] = `I'm Dennis Reynolds 2`;
+      // this.state.res1[0]['quotes'][2] = `I'm Dennis Reynolds 2`;
     })
   }
 
   setQuoteCharacter() {
-    console.log('test');
+
   }
 
   generateQuote() {
-    return 'Throw me in the traaaaaaaaaaaash';
+
   }
 
   render() {
@@ -43,11 +79,11 @@ export default class Cast extends React.Component {
 
         {/* Character Cards */}
         <div className="flex flex-wrap justify-center tc">
-          {this.state.res1.map(({ name, character, profile_path }, index) =>{
+          {this.state.res1.map(({ name, character, profile_path, quote }, index) =>{
             const x = `https://image.tmdb.org/t/p/w600_and_h900_bestv2${profile_path}`;
 
             return (
-              // Individual Cards - Front
+            // Individual Cards - Front
             <div key={-index} className="w-30 pa3 mr2 ma1 heightContain flip-card tc" onMouseEnter={this.generateQuote}>
               <div className="flip-card-inner tc">
 
@@ -57,10 +93,20 @@ export default class Cast extends React.Component {
                   <span className="bigGrowth">{`Character: ${character}`}</span></p>
                   <img key={index+character} alt={name} width="25%" max-width="300px" src={x} />
                 </div>
-              <div className="flip-card-back">
-                <h1>{`- ${character}`}</h1>
-                <p className="quoteText">{this.generateQuote()}</p>
-              </div>
+
+                {/* Back Card Details */}
+                <div className="flip-card-back">
+
+                  {/* generate quote from list of quotes */}
+                  <h3>{ 
+                    index === 0 ? this.state.quotesDennis[Math.floor(Math.random() * this.state.quotesDennis.length)] : 
+                    index === 1 ? this.state.quotesCharlie[Math.floor(Math.random() * this.state.quotesCharlie.length)] : 
+                    index === 2 ? this.state.quotesDeandra[Math.floor(Math.random() * this.state.quotesDeandra.length)] : 
+                    index === 3 ? this.state.quotesFrank[Math.floor(Math.random() * this.state.quotesFrank.length)] :
+                    index === 4 ? this.state.quotesRonald[Math.floor(Math.random() * this.state.quotesRonald.length)] : ''
+                    }</h3>
+                  <h1>{`- ${character}`}</h1>
+                </div>
               </div>
             </div>
             )}
